b9623f0c8dfcd9b54e78a756d4dbdc92306df71d
Alexander Arvelo <alexander.arvelo2390@gmail.com>
2025-10-03 14:56:13 -0300

    Correo contacto 1

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-export async function POST(req: Request) {
+export async function POST(req:Request) {
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   try {
